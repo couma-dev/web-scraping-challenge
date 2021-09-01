bf40c56b14facdd4d2435136a648ff8edcbfdff2
Clifford Ouma <maxcliff9@gmail.com>
2021-09-01 00:35:14 -0400

    final homework commit

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,23 +1,29 @@
-from flask import Flask, jsonify, render_template, redirect
-
+from flask import Flask, render_template, redirect
+import pymongo
+import scrape_mars
 
 #################################################
 # Flask Setup
 #################################################
 app = Flask(__name__)
 
-
+mongo = pymongo(app, url="mongodb://localhost:27017/mars_app")
 #################################################
 # Flask Routes
 #################################################
 
-@app.route("/")
+@app.route("/scrape")
 
-def welcome():
+def home():
     """List all available api routes."""
-    return (
+    mars_dict= mongo.db.mars_dict.find_one()
+    return render_template ('index.html', mars = mars_dict)
       
-    )
-
+@app.route("/scrape")
+def scrape():
+    mars_dict = mongo.db.mars_dict
+    mars_data = scrape_mars.scrape()
+    mars_dict.update({}, mars_data, upsert=True)
+    return redirect("/")
 if __name__ == '__main__':
     app.run(debug=True)
