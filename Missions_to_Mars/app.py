b6fae3494fb1b398cea65292ee540cc999fa665f
Clifford Ouma <maxcliff9@gmail.com>
2021-09-16 23:43:40 -0400

    final app.py commits

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,5 +1,6 @@
+
 from flask import Flask, render_template, redirect
-import pymongo
+from flask_pymongo import PyMongo
 import scrape_mars
 
 #################################################
@@ -7,8 +8,10 @@ import scrape_mars
 #################################################
 app = Flask(__name__)
 
-mongo = pymongo(app, url="mongodb://localhost:27017/mars_app")
-mars_data = mongo.db.mars_data
+#mongo = pymongo(app, url="mongodb://localhost:27017/mars_app")
+#mars_data = mongo.db.mars_data
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 #################################################
 # Flask Routes
 #################################################
@@ -17,17 +20,15 @@ mars_data = mongo.db.mars_data
 
 def home():
     """List all available api routes."""
-    return render_template ('index.html')
+    scraped_dict = mongo.db.scraped_dict.find_one()
+    return render_template ("index.html", mars = scraped_dict)
       
 @app.route("/scrape")
 def scrape():
-     
+     scraped_dict = mongo.db.scraped_dict
      scraped_data = scrape_mars.scrape()
-     mars_data.update({}, scraped_data, upsert=True)
-     return redirect("/data")
-@app.route("/data")
-def data():
-    mars_info = mongo.db.mars_data.find_one()
-    return render_template("index.html", info=mars_info)
+     scraped_dict.update({}, scraped_data, upsert=True)
+     return redirect("/", code = 302)
+
 if __name__ == '__main__':
     app.run(debug=True)
